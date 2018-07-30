1734be4a24cdfdac3ca29f333db40618e15ca6e8
atective <41925047+atective@users.noreply.github.com>
2018-07-30 12:08:00 -0700

    Add files

---
+++ app.js
@@ -0,0 +1,33 @@
+const search = instantsearch({
+  appId:'ZHGXSA9UHQ',
+  apiKey:'547423bf8184758030eebead641790ad',
+  indexName:'products',
+  routing: true,
+  searchParameters: {
+    hitsPerPage: 10
+  }
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search-input'
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: document.getElementById('hit-template').innerHTML,
+      empty: "We didn't find any results for the search <em>\"{{query}}\"</em>"
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination'
+  })
+);
+
+search.start();
